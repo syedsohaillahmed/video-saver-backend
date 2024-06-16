b8c789c8959da0607374b8e910e5cb6d689c3036
syedsohaillahmed <syedsohaillahmed19@gmail.com>
2024-06-16 22:33:21 +0530

    multer and cloudinary setup completed

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null; // if not localpath send null
+
+    // uploader.upload tkes 2 params file path and specifications
+    const cloudinaryResponse = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("Succesfull uploaded response", cloudinaryResponse.url);
+    return cloudinaryResponse;
+  } catch (error) {
+    // if upload fails unlinking file fs is filesystem of node availabele inbuilt
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export{uploadOnCloudinary}
